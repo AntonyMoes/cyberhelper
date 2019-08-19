7fcbd4e8cce19343e1a35ffaf0802f4d88fe3a93
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2019-08-19 23:01:35 +0300

    ADS, MOAR

--- bot.py
+++ bot.py
@@ -6,15 +6,18 @@ from random import randint, choice
 from string import ascii_lowercase
 
 from bot_utils.quotes import get_quote
+from bot_utils.ads import get_ad
 from generator import Generator
 
 generator = Generator('trained_data/cyber_weights')
 
 
 def process_command(message: str) -> str:
-    print('Message:', message)
+    is_ad = randint(1, 50) == 1
+    if is_ad:
+        return get_ad()
+
     if message.find('цитата') != -1:
-        print('ЩА БУДУ ЦИТИРОВАТЬ')
         return get_quote()
     else:
         return generator.generate(seed=message, size=randint(10, 80))
