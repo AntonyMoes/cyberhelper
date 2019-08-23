57e12702ed5255ece7b2f18b98f1e3cc5c2c6223
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2019-08-23 13:47:08 +0300

    Fixed deploy issues

--- run_bot.py
+++ run_bot.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     except KeyError:
         exit(1)
 
-    pg_user = get_env_var('PG_USER', 'postgres')
-    pg_password = get_env_var('PG_PASSWORD', '')
-    pg_database = get_env_var('PG_DATABASE', 'postgres')
+    pg_user = get_env_var('POSTGRES_USER', 'postgres')
+    pg_password = get_env_var('POSTGRES_PASSWORD', '')
+    pg_database = get_env_var('POSTGRES_DB', 'postgres')
     pg_host = get_env_var('PG_HOST', 'localhost')
 
     bot = Bot(token, pg_user=pg_user, pg_password=pg_password, pg_database=pg_database, pg_host=pg_host)
