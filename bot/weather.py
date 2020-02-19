05c31ebf162234da03ac91fe3939a59050bbc3f5
AntonyMo <moantonymo@gmail.com>
2020-02-19 23:26:37 +0300

    fixed weather

--- bot/weather.py
+++ bot/weather.py
@@ -1,5 +1,6 @@
 from urllib.parse import quote
-import aiohttp
+from aiohttp import request
+import asyncio
 from selectolax.parser import HTMLParser
 
 from utils import user_agent
@@ -9,7 +10,7 @@ weather_request = '/pogoda/search?request='
 
 
 async def get_weather(location: str) -> str:
-    async with aiohttp.request('GET', domain + weather_request + quote(location), headers={'User-Agent': user_agent}) as resp:
+    async with request('GET', domain + weather_request + quote(location), headers={'User-Agent': user_agent}) as resp:
         search_text = await resp.text()
         title = HTMLParser(search_text).css_first('title').text()
         possible_href = str(resp.url)
@@ -31,7 +32,7 @@ async def get_weather(location: str) -> str:
         node = node.css_first('a')
         href = domain + node.attributes['href']
         exact_location = node.text()
-        async with aiohttp.request('GET', href, headers={'User-Agent': user_agent}) as resp:
+        async with request('GET', href, headers={'User-Agent': user_agent}) as resp:
             weather_text = await resp.text()
 
     # parsing weather
@@ -39,10 +40,10 @@ async def get_weather(location: str) -> str:
     temp_info = card.css_first('div.fact__temp-wrap').css_first('a')
     now_temp = temp_info.css_first('div.fact__temp').css_first('span.temp__value').text()
     now_condition = temp_info.css_first('div.fact__feelings').css_first('div.link__condition').text()
-    wind_info = card.css_first('div.fact__props').css_first('dl.fact__wind-speed').css_first('dd.term__value')
+    wind_info = card.css_first('div.fact__props').css_first('div.fact__wind-speed').css_first('div.term__value')
     now_wind = wind_info.css_first('span.wind-speed').text() + ' ' + wind_info.css_first('span.fact__unit').text()
 
-    day_info = HTMLParser(weather_text).css_first('div.forecast-briefly').css_first('div.swiper-wrapper')
+    day_info = HTMLParser(weather_text).css_first('div.forecast-briefly').css_first('ul.swiper-wrapper')
     # print(day_info.html)
     slide = None
     for day in day_info.css('div.swiper-slide'):
@@ -58,3 +59,6 @@ async def get_weather(location: str) -> str:
            f'\n\nCЕЙЧАС:\nТемпература: {now_temp}\nСостояние: {now_condition}\nВетер: {now_wind}' \
            f'\n\nCЕГОДНЯ:\nТемпература днем: {day_temp}\nТемпература ночью: {night_temp}\nСостояние: {condition}'\
            f'\n\nПолный прогноз: {href}'
+
+if __name__ == '__main__':
+    print(asyncio.get_event_loop().run_until_complete(get_weather('москва')))
